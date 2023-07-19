52ffda86b04043ab4eb25fc31455bb0d5adca299
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-19 06:38:43 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split (' ');
+console.log (moo, neigh, oink,cluck);
+const [bessie, ,dolly, babe, little] = farmAnimals.split (' ');
+console.log (bessie, dolly, babe, little);
+const [blackAndWhite, ,black, pink,] = farmAnimals.split (' ');
+console.log(blackAndWhite, black, pink);
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r,o,y,g,b,,v] = colors;
+const [,,,,,indg,] = colors;
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +16,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const {muppetName, color, song, job, partner} = muppet;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +33,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const {song2,song4} = nestedMuppet.album.theMuppetMovie;
+const {nestedJob,nestedPartner} = nestedMuppet;
 
 // Strings
 
